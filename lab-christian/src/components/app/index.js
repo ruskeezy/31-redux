8910e34eb0615a4a4ff0381f05e3b705a3833ed7
Christian Miller <christian.ax.miller@gmail.com>
2018-03-26 21:32:01 -0700

    app

---
+++ lab-christian/src/components/app/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import React from 'react';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Dashboard from '../dashboard/index';
+import CreateAppStore from '../../lib/store';
+
+const store = CreateAppStore();
+
+class App extends React.Component {
+  componentDidMount() {
+    store.subscribe(() => {
+      console.log('__STATE', store.getState());
+    });
+
+    store.dispatch({ type: null });
+  }
+
+  render() {
+    return (
+      <section>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Route exact path='/' component={Dashboard} />
+          </BrowserRouter>
+        </Provider>
+      </section>
+    );
+  }
+}
+
+export default App;
