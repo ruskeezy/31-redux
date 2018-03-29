30f9f5dc8a81385dc68851ab1c3bf37856395763
Christian Miller <christian.ax.miller@gmail.com>
2018-03-28 22:21:24 -0700

    required in /exported from store

--- lab-christian/src/lib/store.js
+++ lab-christian/src/lib/store.js
@@ -1,6 +1,9 @@
 'use strict';
 
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from '../reducer/index';
+import reporter from './reporter';
+import validateCategory from './validate-category';
+import validateExpense from './validate-expense';
 
-export default () => createStore(reducer);
+export default () => createStore(reducer, applyMiddleware(reporter, validateCategory, validateExpense));
